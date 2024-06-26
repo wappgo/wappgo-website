1b6a022cbba66a6630ead6d886c6f5b6b2edf717
AnuragDWappgo <149356959+AnuragDWappgo@users.noreply.github.com>
2024-06-26 14:39:24 +0530

    fix the modules error

--- app/services/design/promotion-product-video/page.jsx
+++ app/services/design/promotion-product-video/page.jsx
@@ -1,9 +1,9 @@
-import AnimateText from '../../../../../components/AnimateText'
-import Announcement from '../../../../../components/Announcement'
-import Navbar from '../../../../../components/Navbar'
-import Footer from '../../../../../components/Footer'
-import InnovationHero from '../../../../../components/Innovation-Hero'
-import InnovationService from '../../../../../components/Innovation-Service'
+import AnimateText from '../../../../components/AnimateText'
+import Announcement from '../../../../components/Announcement'
+import Navbar from '../../../../components/Navbar'
+import Footer from '../../../../components/Footer'
+import InnovationHero from '../../../../components/Innovation-Hero'
+import InnovationService from '../../../../components/Innovation-Service'
 
 
 const PromotionVideo = () => {
